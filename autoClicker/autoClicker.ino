7c18cbd6fbbf8062c9b2c04212a4467353d60e95
Andreas <1990andreaslindblad@gmail.com>
2024-11-10 13:43:48 +0100

    1.0
    1.0 update. it works as intended in its case

--- autoClicker/autoClicker.ino
+++ autoClicker/autoClicker.ino
@@ -3,27 +3,33 @@
 Servo myServo;
 
 int potVal;
-int choice;
+int baseWait = 20000;
+int programClick = 75;
+
+int clickCount = 0;
 
 void setup() {
-  myServo.attach(5);
-
-
+  myServo.attach(9);
 }
 
 void loop() {
+  if (clickCount < programClick) {
+    int randomDelay = baseWait + random(-3000, 3000);
+    potVal = 180;
 
-  // Cycle
-  potVal = 160; // Push contact
-  
-  myServo.write(potVal);  
+    myServo.write(potVal);  
+    delay(500);
 
-  delay(30000);//ms, wait time
-  
-  potVal = 90; // Neutral
-  
-  myServo.write(potVal);  
+    potVal = 110;
+    myServo.write(potVal);
 
-  delay(200); //ms
-  //Cycle off
+    delay(randomDelay);
+    clickCount++;  // Här är semikolonet tillagt
+  }
+  else {
+    myServo.detach(); // Koppla bort servon
+    while (true) {
+      // Arduino stannar här, gör inget mer
+    } 
+  }
 }
